1d9e3726ccd92144b2303211c43d30dc935a04dc
Marina Zujic <zujicmarina@gmail.com>
2020-09-27 21:10:24 +0200

    About section icons, hero image fix heights, grid changed gaps

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const darkModeToggler = document.querySelector('.nav__items-toggler')
-
 const darkModeBtn = document.querySelector('.nav__items-btn')
 
 darkModeToggler.addEventListener('click', () => {
-    darkModeBtn.classList.toggle('clicked')
-    darkModeToggler.classList.toggle('dark')
+
+    darkModeBtn.classList.toggle('clicked');
+    darkModeToggler.classList.toggle('dark');
+    // document.body.classList.toggle('dark-mode');
+
+    // document.body.classList.contains('dark-mode') ?
+    //     darkModeBtn.classList.toggle('clicked') :
+    //     darkModeToggler.classList.toggle('dark');
 })
